f0d84af028be7dd480d1f401cb2a4a8795ce199b
Karl McPhee <karmcphee@cox.net>
2023-11-27 09:42:43 -0500

    Initial Commit

---
+++ CryptoPriceViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoPriceViewer
+//
+//  Created by Karl McPhee on 11/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoPriceViewer/CryptoPriceViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPriceViewerApp.swift
+//  CryptoPriceViewer
+//
+//  Created by Karl McPhee on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPriceViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
